8be1dd8bde4a6e90178994c1f3fd2b001a902993
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-19 01:58:39 +0600

    custom btn css modified

--- src/Pages/Shared/CustomBtn/CustomBtn.jsx
+++ src/Pages/Shared/CustomBtn/CustomBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CustomBtn = ({text}) => {
     return (
         <div>
-            <button className='bg-cyan-300 py-2 px-3 rounded-lg hover:text-white hover:bg-cyan-600 hover:animate-pulse'>{text}</button>
+            <button className='inline-flex w-full justify-center rounded-lg bg-cyan-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900 hover:animate-pulse'>{text}</button>
         </div>
     );
 };
