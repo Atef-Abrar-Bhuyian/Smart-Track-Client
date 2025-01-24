2c8883a1866c688797d147db1a7923e16dde9440
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-25 02:34:48 +0600

    hr assets fetch to team members users

--- src/Pages/AssetsRequest/AssetsRequest.jsx
+++ src/Pages/AssetsRequest/AssetsRequest.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { Table } from "flowbite-react";
 
 const AssetsRequest = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: requestAssets = [] } = useQuery({
+    queryKey: ["requestAssets", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`requestAssets/${user?.email}`);
+      return res.data;
+    },
+  });
+
+  return (
+    <div className="w-11/12 mx-auto my-10">
+      <div className="overflow-x-auto">
+        {requestAssets.length > 0 ? (
+          <Table striped>
+            <Table.Head>
+              <Table.HeadCell>Asset Name</Table.HeadCell>
+              <Table.HeadCell>Asset Type</Table.HeadCell>
+              <Table.HeadCell>Availability </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {requestAssets.map((asset) => (
+                <Table.Row
+                  key={asset._id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {asset?.productName}
+                  </Table.Cell>
+                  <Table.Cell>{asset?.productType}</Table.Cell>
+                  {/* <Table.Cell>{`${asset?.quantity > 0} ? `}</Table.Cell> */}
+                  <Table.Cell>
+                    {asset?.quantity > 0 ? (
+                      <p className="text-green-500">Instock</p>
+                    ) : (
+                      <p className="text-red-500">Stock Out</p>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <button className="inline-flex justify-center rounded-lg bg-cyan-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900 ">
+                      Request
+                    </button>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        ) : (
+          <h1 className="text-2xl font-bold text-center">
+            No assets available.
+          </h1>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AssetsRequest;
--- src/Pages/EmployeeTeam/EmployeeTeam.jsx
+++ src/Pages/EmployeeTeam/EmployeeTeam.jsx
@@ -7,9 +7,8 @@ import { Table } from "flowbite-react";
 const EmployeeTeam = () => {
   const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
-  
 
-  const { data: employees = [], refetch } = useQuery({
+  const { data: employees = [] } = useQuery({
     queryKey: ["emplyeeTeam", user?.email],
     enabled: !loading,
     queryFn: async () => {
@@ -17,29 +16,40 @@ const EmployeeTeam = () => {
       return res.data;
     },
   });
-console.log(employees?.team?.employees);
-  return <div className="md:w-6/12 mx-auto my-10">
-    <div className="my-10">
-      <h1 className="text-2xl text-center font-bold">My Team Members</h1>
-    </div>
-    <Table striped>
+  
+  return (
+    <div className="md:w-6/12 mx-auto my-10">
+      <div className="my-10">
+        <h1 className="text-2xl text-center font-bold">My Team Members</h1>
+      </div>
+      <Table striped>
         <Table.Head>
           <Table.HeadCell>Employee Name</Table.HeadCell>
           <Table.HeadCell>Image</Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          {
-            employees?.team?.employees.map(team => <Table.Row key={team?.employee_id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          {employees?.team?.employees.map((team) => (
+            <Table.Row
+              key={team?.employee_id}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                 {team?.employeeName}
               </Table.Cell>
-              <Table.Cell> <img className="w-20 h-20 border-none rounded-full" src={team?.employeePhoto} alt={`${team?.employeeName}'s Photo`} /> </Table.Cell>
-            </Table.Row>)
-          }
-          
+              <Table.Cell>
+                {" "}
+                <img
+                  className="w-20 h-20 border-none rounded-full"
+                  src={team?.employeePhoto}
+                  alt={`${team?.employeeName}'s Photo`}
+                />{" "}
+              </Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
-  </div>;
+    </div>
+  );
 };
 
 export default EmployeeTeam;
